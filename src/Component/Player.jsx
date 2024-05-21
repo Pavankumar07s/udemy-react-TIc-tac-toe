be39de67898a2da4f475f2bb5c933d78089dddaf
Pavankumar07s <pawankumar14662693@gmail.com>
2024-05-22 00:01:13 +0530

    player dynamic input added

--- src/Component/Player.jsx
+++ src/Component/Player.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 export default function Player({name,symbol}){
+   let [isEditing,setIsEditing]=useState(false);
+   const playerNameEdit=()=>{
+        setIsEditing(true);
+   }
     return(
         <>
         <li>
             <span className="player">
-            <span className="player-name">Player {name}</span>
+            {isEditing?<input type="text" required></input>:<span className="player-name">Player {name}</span>}
+            
             <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={()=>playerNameEdit()}>Edit</button>
           </li>
         </>
 
